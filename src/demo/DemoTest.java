d0ed9edf619398b3104e958ccbfc16db560c0d9d
bsal4 <bsal4@DESKTOP-A6CO3BH>
2022-08-02 14:41:52 -0400

    for us123

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -3,5 +3,11 @@ package demo;
 public class DemoTest {
 public static void main(String[] args) {
 	System.out.println();
+ 
+	
+}
+//for us123
+public static void add(int a, int b) {
+	System.out.println(a+b);
 }
 }
